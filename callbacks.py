ea798f16ebeadff4212672b80aae0dabdbdd7727
Lê Hoàng Phương Nhi <91231366+lhpnhi@users.noreply.github.com>
2022-05-23 18:13:04 +0700

    Add files via upload

--- callbacks.py
+++ callbacks.py
@@ -8,38 +8,45 @@ import warnings
 
 
 class Callback:
+    """
+    Abstract base class used to build new callbacks.
+    """
+
     def __init__(self):
         pass
 
     def set_params(self, params):
         self.params = params
-    
+
     def set_trainer(self, model):
         self.trainer = model
 
-    def on_epoch_begin(self, epoch, logs = None):
+    def on_epoch_begin(self, epoch, logs=None):
         pass
 
-    def on_epoch_end(self, epoch, logs = None):
+    def on_epoch_end(self, epoch, logs=None):
         pass
 
-    def on_batch_begin(self, batch, logs = None):
+    def on_batch_begin(self, batch, logs=None):
         pass
 
-    def on_batch_end(self, batch, logs = None):
+    def on_batch_end(self, batch, logs=None):
         pass
 
-    def on_train_begin(self, logs = None):
+    def on_train_begin(self, logs=None):
         pass
 
-    def on_train_end(self, logs = None):
+    def on_train_end(self, logs=None):
         pass
+
 
 @dataclass
 class CallbackContainer:
-    """Container giữ danh sách callbacks"""
+    """
+    Container holding a list of callbacks.
+    """
 
-    callbacks: List[Callback] = field(default_factory = list)
+    callbacks: List[Callback] = field(default_factory=list)
 
     def append(self, callback):
         self.callbacks.append(callback)
@@ -53,45 +60,61 @@ class CallbackContainer:
         for callback in self.callbacks:
             callback.set_trainer(trainer)
 
-    def on_epoch_begin(self, epoch, logs = None):
+    def on_epoch_begin(self, epoch, logs=None):
         logs = logs or {}
         for callback in self.callbacks:
-            callback.on_batch_begin(epoch, logs)
+            callback.on_epoch_begin(epoch, logs)
 
-    def on_epoch_end(self, epoch, logs = None):
+    def on_epoch_end(self, epoch, logs=None):
         logs = logs or {}
         for callback in self.callbacks:
             callback.on_epoch_end(epoch, logs)
 
-    def on_batch_begin(self, batch, logs = None):
+    def on_batch_begin(self, batch, logs=None):
         logs = logs or {}
         for callback in self.callbacks:
             callback.on_batch_begin(batch, logs)
 
-    def on_batch_end(self, batch, logs = None):
+    def on_batch_end(self, batch, logs=None):
         logs = logs or {}
         for callback in self.callbacks:
-            callback.on_batch_end
+            callback.on_batch_end(batch, logs)
 
-    def on_train_begin(self, logs = None):
+    def on_train_begin(self, logs=None):
         logs = logs or {}
         logs["start_time"] = time.time()
         for callback in self.callbacks:
             callback.on_train_begin(logs)
 
-    def on_train_end(self, logs = None):
+    def on_train_end(self, logs=None):
         logs = logs or {}
         for callback in self.callbacks:
             callback.on_train_end(logs)
 
+
 @dataclass
 class EarlyStopping(Callback):
-    """Thoát khỏi vòng lặp nếu không cải thiện"""
+    """EarlyStopping callback to exit the training loop if early_stopping_metric
+    does not improve by a certain amount for a certain
+    number of epochs.
+    Parameters
+    ---------
+    early_stopping_metric : str
+        Early stopping metric name
+    is_maximize : bool
+        Whether to maximize or not early_stopping_metric
+    tol : float
+        minimum change in monitored value to qualify as improvement.
+        This number should be positive.
+    patience : integer
+        number of epochs to wait for improvement before terminating.
+        the counter be reset after each improvement
+    """
 
-    early_stopping_metric: str #Độ đo để sử dụng dừng huấn luyện sớm
-    is_maximize: bool 
-    tol: float = 0.0 #Giá trị tối thiểu để xác định xem kết quả có cải thiện không
-    patience: int = 5 #Số epoch chờ đợi cải thiện trước khi dừng sớm
+    early_stopping_metric: str
+    is_maximize: bool
+    tol: float = 0.0
+    patience: int = 5
 
     def __post_init__(self):
         self.best_epoch = 0
@@ -103,21 +126,21 @@ class EarlyStopping(Callback):
             self.best_loss = -self.best_loss
         super().__init__()
 
-    def on_epoch_end(self, epoch, logs = None):
+    def on_epoch_end(self, epoch, logs=None):
         current_loss = logs.get(self.early_stopping_metric)
         if current_loss is None:
             return
-        
+
         loss_change = current_loss - self.best_loss
-        max_improved = self.is_maximize and loss_change > self.tol 
+        max_improved = self.is_maximize and loss_change > self.tol
         min_improved = (not self.is_maximize) and (-loss_change > self.tol)
         if max_improved or min_improved:
             self.best_loss = current_loss
             self.best_epoch = epoch
             self.wait = 1
-            self.best_weights = copy.deepcopy(self.trainer.network.state_dict()) #Lưu lại trọng số tốt nhất (cho loss thấp nhất)
+            self.best_weights = copy.deepcopy(self.trainer.network.state_dict())
         else:
-            if self.wait >= self.patience: #Nếu số lần đợi vượt quá hoặc bằng với số lần đợi tối đa thì dừng training
+            if self.wait >= self.patience:
                 self.stopped_epoch = epoch
                 self.trainer._stop_training = True
             self.wait += 1
@@ -146,8 +169,20 @@ class EarlyStopping(Callback):
         wrn_msg = "Best weights from best epoch are automatically used!"
         warnings.warn(wrn_msg)
 
+
 @dataclass
-class History(Callback): #Ghi lại các sự kiện (các thay đổi) vào History
+class History(Callback):
+    """Callback that records events into a `History` object.
+    This callback is automatically applied to
+    every SuperModule.
+    Parameters
+    ---------
+    trainer : DeepRecoModel
+        Model class to train
+    verbose : int
+        Print results every verbose iteration
+    """
+
     trainer: Any
     verbose: int = 1
 
@@ -167,7 +202,7 @@ class History(Callback): #Ghi lại các sự kiện (các thay đổi) vào His
         self.epoch_metrics = {"loss": 0.0}
         self.samples_seen = 0.0
 
-    def on_epoch_end(self, epoch, logs = None):
+    def on_epoch_end(self, epoch, logs=None):
         self.epoch_metrics["loss"] = self.epoch_loss
         for metric_name, metric_value in self.epoch_metrics.items():
             self.history[metric_name].append(metric_value)
@@ -180,10 +215,10 @@ class History(Callback): #Ghi lại các sự kiện (các thay đổi) vào His
             if metric_name != "lr":
                 msg += f"| {metric_name:<3}: {np.round(metric_value, 5):<8}"
         self.total_time = int(time.time() - self.start_time)
-        msg += f"| {str(datetime.timedelta(seconds = self.total_time)) + 's':<6}"
+        msg += f"|  {str(datetime.timedelta(seconds=self.total_time)) + 's':<6}"
         print(msg)
 
-    def on_batch_end(self, batch, logs = None):
+    def on_batch_end(self, batch, logs=None):
         batch_size = logs["batch_size"]
         self.epoch_loss = (
             self.samples_seen * self.epoch_loss + batch_size * logs["loss"]
@@ -199,30 +234,42 @@ class History(Callback): #Ghi lại các sự kiện (các thay đổi) vào His
     def __str__(self):
         return str(self.history)
 
+
 @dataclass
-class LRSchedulerCallback(Callback): #Kiểm soát tốc độ học tập (kiểu như learning rate),
-                                     #kiểm soát tốc độ mô hình thay đổi các trọng số để phù hợp với bài toán
+class LRSchedulerCallback(Callback):
+    """Wrapper for most torch scheduler functions.
+    Parameters
+    ---------
+    scheduler_fn : torch.optim.lr_scheduler
+        Torch scheduling class
+    scheduler_params : dict
+        Dictionnary containing all parameters for the scheduler_fn
+    is_batch_level : bool (default = False)
+        If set to False : lr updates will happen at every epoch
+        If set to True : lr updates happen at every batch
+        Set this to True for OneCycleLR for example
+    """
+
     scheduler_fn: Any
     optimizer: Any
     scheduler_params: dict
     early_stopping_metric: str
-    is_batch_level: bool = False 
+    is_batch_level: bool = False
 
     def __post_init__(
         self,
     ):
-
         self.is_metric_related = hasattr(self.scheduler_fn, "is_better")
         self.scheduler = self.scheduler_fn(self.optimizer, **self.scheduler_params)
         super().__init__()
 
-    def on_batch_end(self, batch, logs = None):
+    def on_batch_end(self, batch, logs=None):
         if self.is_batch_level:
             self.scheduler.step()
         else:
             pass
 
-    def on_epoch_end(self, epoch, logs = None):
+    def on_epoch_end(self, epoch, logs=None):
         current_loss = logs.get(self.early_stopping_metric)
         if current_loss is None:
             return
